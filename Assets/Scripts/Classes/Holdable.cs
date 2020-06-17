5c44aa726b4013c97946aa29ccd8b12413d193a2
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-06-17 14:30:40 +0200

    item classes

---
+++ Assets/Scripts/Classes/Holdable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Holdable Item",menuName = "Game/Item/Holdable")]
+public class Holdable : Item
+{
+    public GameObject model;
+
+    public void Main()
+    {
+
+    }
+    public void Secondary()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Classes/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Game/Item")]
+public class Item : ScriptableObject
+{
+    public Sprite icon;
+    public new string name;
+    public string description;
+    public int maxStackCount;
+}
