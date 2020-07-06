72bde914804daa6428595e708ae12960e79b3bf1
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-07-06 15:16:24 +0200

    inventory prototype

---
+++ Assets/Scripts/Classes/Stack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stack : MonoBehaviour
+{
+    public Item item;
+    public int count;
+
+    public Stack(Item item, int v)
+    {
+        this.item = item;
+        this.count = v;
+    }
+}
---
+++ Assets/Scripts/Misc/InventorySystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public Stack[] stacks;
+
+    public int inventorySize = 20;
+    private void Start()
+    {
+        stacks = new Stack[inventorySize];
+    }
+
+    public void AddItem(Item item)
+    {
+        for (int i = 0; i < stacks.Length; i++)
+        {
+            if(stacks[i] != null)
+            {
+                if(stacks[i].item == item)
+                {
+                    if(stacks[i].count < stacks[i].item.maxStackCount)
+                    {
+                        stacks[i].count++;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                stacks[i] = new Stack(item, 1);
+            }
+        }
+    }
+}
