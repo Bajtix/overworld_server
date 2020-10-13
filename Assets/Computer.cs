2b9d382bfc90b1f3abc84bbf5126ac17c1598cfc
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-10-13 23:01:32 +0200

    change to js, computer entities progress

---
+++ Assets/Computer.cs
@@ -0,0 +1,25 @@
+﻿using Jint;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour
+{
+    public string actualConsoleLog;
+    private string display;
+
+    public string inputBuffer;
+
+    public Engine javascript;
+
+    private void Start()
+    {
+        javascript = new Engine();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/NativeJSMethods.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NativeJSMethods
+{
+    public Computer computerBind;
+
+    public ConsoleLib console;
+
+    public NativeJSMethods(Computer computer)
+    {
+        computerBind = computer;
+        
+    }
+    public class JSLIB
+    {
+        public Computer computerBind;
+
+        public JSLIB(Computer computerBind)
+        {
+            this.computerBind = computerBind;
+        }
+    }
+
+    public class ConsoleLib : JSLIB
+    {
+        public ConsoleLib(Computer computerBind) : base(computerBind) { }
+        
+        public void printl(string text)
+        {
+            computerBind.actualConsoleLog += "\n" + text;
+        }
+
+        public void print(string text)
+        {
+            computerBind.actualConsoleLog += text;
+        }
+    }
+}
